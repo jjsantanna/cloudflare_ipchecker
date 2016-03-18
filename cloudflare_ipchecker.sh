872ac2f76921aa9d1ff66516263eba8517317cf5
Jair Santanna <jairsantanna@gmail.com>
2016-03-18 21:46:08 +0100

    update

--- cloudflare_ipchecker.sh
+++ cloudflare_ipchecker.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-whois $1 |grep -ci Cloudflare|awk '{if ($0 > 0) print "Y"; else print "N"}'
+output=`whois $1 |grep -ci Cloudflare|awk '{if ($0 > 0) print "Y"; else print "N"}'`
+echo "$1;$output"
